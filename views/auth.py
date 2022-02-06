9407d08192fe9a2510259aa4b679a1d9f989709c
Elias Herranen <elias.herranen@gmail.com>
2022-02-06 22:13:31 +0200

    fixing and enhancing input register and settings page client side validations

--- views/auth.py
+++ views/auth.py
@@ -3,9 +3,9 @@ from services import auth
 from utils.constant import DANGER_CATEGORY, SUCCESS_CATEGORY
 from utils.validators.auth_validator import requires_login
 
-LOGIN_SUCCESS_MESSAGE = "You successfully logged in!"
-LOGIN_ERROR_MESSAGE = "Wrong username or password"
-SUCCESSFULLY_LOGGED_OUT_MESSAGE = "You successfully logged out!"
+LOGIN_SUCCESSFULLY_MESSAGE = "You have successfully logged in!"
+LOGGED_OUT_SUCCESSFULLY_MESSAGE = "You have successfully logged out!"
+LOGIN_ERROR_MESSAGE = "Wrong username or password!"
 
 auth_bp = Blueprint("auth", __name__)
 
@@ -27,18 +27,16 @@ def process_login():
     if logged_user_info:
         session["user_id"] = logged_user_info.id
         session["is_doctor"] = logged_user_info.is_doctor
-        flash(LOGIN_SUCCESS_MESSAGE, SUCCESS_CATEGORY)
+        flash(LOGIN_SUCCESSFULLY_MESSAGE, SUCCESS_CATEGORY)
         return redirect("/profile")
 
     flash(LOGIN_ERROR_MESSAGE, DANGER_CATEGORY)
     return redirect("/login")
-
 
 @auth_bp.route("/logout", methods=["POST"])
 @requires_login
 def logout():
     del session["user_id"]
     del session["is_doctor"]
-    flash(SUCCESSFULLY_LOGGED_OUT_MESSAGE, SUCCESS_CATEGORY)
+    flash(LOGGED_OUT_SUCCESSFULLY_MESSAGE, SUCCESS_CATEGORY)
     return redirect("/login")
-
