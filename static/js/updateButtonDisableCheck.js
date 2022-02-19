8808f3ab1e908e041f3eef75a1f21e2c53e8ce46
Elias Herranen <elias.herranen@gmail.com>
2022-02-19 16:35:39 +0200

    Refactored settings page input validation

--- static/js/updateButtonDisableCheck.js
+++ static/js/updateButtonDisableCheck.js
@@ -1,24 +1,22 @@
 const isEmpty = str => !str.trim().length;
 
-document.querySelectorAll('.container').forEach(item => {
-    item.addEventListener('input', event => {
-        const nameValue = document.getElementById("name").value;
-        const emailValue = document.getElementById("email").value;
-        const phoneValue = document.getElementById("phone").value;
-        const addressValue = document.getElementById("address").value;
-        const cityValue = document.getElementById("city").value;
-        const countryValue = document.getElementById("country").value;
+document.addEventListener('input', event => {
+    const nameValue = document.getElementById("name").value;
+    const emailValue = document.getElementById("email").value;
+    const phoneValue = document.getElementById("phone").value;
+    const addressValue = document.getElementById("address").value;
+    const cityValue = document.getElementById("city").value;
+    const countryValue = document.getElementById("country").value;
 
-        const updateButton = document.getElementById('updateButton')
-        if (isEmpty(nameValue) &&
-            isEmpty(emailValue) &&
-            isEmpty(phoneValue) &&
-            isEmpty(addressValue) &&
-            isEmpty(cityValue) &&
-            isEmpty(countryValue)) {
-            updateButton.disabled = true
-        } else {
-            updateButton.disabled = false
-        }
-    })
+    const updateButton = document.getElementById('updateButton')
+    if (isEmpty(nameValue) &&
+        isEmpty(emailValue) &&
+        isEmpty(phoneValue) &&
+        isEmpty(addressValue) &&
+        isEmpty(cityValue) &&
+        isEmpty(countryValue)) {
+        updateButton.disabled = true
+    } else {
+        updateButton.disabled = false
+    }
 })
